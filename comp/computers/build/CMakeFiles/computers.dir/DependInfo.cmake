2fd6b1e424dd8a4c01dc41364474987c5a92b47c
alexeysklemin <a76sklemin@gmail.com>
2023-11-21 19:31:40 +0400

    crosses initialization bugs

---
+++ comp/computers/build/CMakeFiles/computers.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/cmkeStuding/comp/computers/scr/cpu.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/cpu.cpp.o"
+  "/home/alex/cmkeStuding/comp/computers/scr/disk.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/disk.cpp.o"
+  "/home/alex/cmkeStuding/comp/computers/scr/gpu.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/gpu.cpp.o"
+  "/home/alex/cmkeStuding/comp/computers/scr/kbd.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/kbd.cpp.o"
+  "/home/alex/cmkeStuding/comp/computers/scr/main.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/main.cpp.o"
+  "/home/alex/cmkeStuding/comp/computers/scr/ram.cpp" "/home/alex/cmkeStuding/comp/computers/build/CMakeFiles/computers.dir/scr/ram.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../hdrs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ comp/computers/build/CMakeFiles/computers.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/computers.dir/scr/cpu.cpp.o"
+  "CMakeFiles/computers.dir/scr/disk.cpp.o"
+  "CMakeFiles/computers.dir/scr/gpu.cpp.o"
+  "CMakeFiles/computers.dir/scr/kbd.cpp.o"
+  "CMakeFiles/computers.dir/scr/main.cpp.o"
+  "CMakeFiles/computers.dir/scr/ram.cpp.o"
+  "computers"
+  "computers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/computers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
