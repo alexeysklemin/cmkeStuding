92b4b0b5843775ea9b13e27956ba3c1008fe3d0d
alexeysklemin <a76sklemin@gmail.com>
2024-02-22 21:26:30 +0400

    disigned class

---
+++ mobile/inc/phone.h
@@ -0,0 +1,24 @@
+#ifndef PHONE_H
+#include <iostream>
+#include <map>
+#include <string>
+#define PHONE_H
+class Phone{
+
+	std::map<std::string, std::string> phoneBook
+
+public:
+	
+	void  add (std::map<std::string, std::string> phoneBook){
+		
+	}
+
+	void call(std::map<std::string, std::string> phoneBook){
+
+	}
+
+	void sms(std::map<std::string, std::string> phoneBook){
+
+	}
+};
+#endif // PHONE_H
