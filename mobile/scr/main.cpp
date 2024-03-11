983e1a0ed30322407dcfc719e33918ffb856abc4
alexeysklemin <a76sklemin@gmail.com>
2024-03-11 21:05:30 +0400

    class is finished

---
+++ mobile/scr/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "phone.h"
+
+int main(){
+
+std::cout<<"Input your wishes: Add contact, do Calling, typing Sms or Exit\n";
+char comm;
+std::cin>>comm;
+
+switch(comm){
+    
+    case 'A':
+        
+        break;
+        
+    case 'C':
+        
+        break;
+        
+    case 'S':
+        
+        break;
+        
+    case 'E':
+        break;
+    
+}
--- mobile/scr/mobile.cpp
+++ mobile/scr/mobile.cpp
@@ -14,29 +14,55 @@ public:
     
         std::cout<<"Imput name of contact to your phone book"<<"\n";
         std::cin>>name;
+        bool incorrectName=false;
         
         for(int i=0; i<name.length(); ++i){
             if(name[i]>='0' and name[i]<=9){
             std::cout<<"Incorrect datas. Repeat please"<<"\n";
-            }else continue
+            incorrectName=true;
+            break;
         }
-    
+        if(incorrectName){
+            continue;
+        }
+        }
         std::cout<<"Input phone number of new contact"<<"\n";
         std::cin>>phone;
         
         if(phone.length()<10 || phone.length()>10) && (phone[0]!='8' || (phone[0]!='+' && phone[1]!='7'){
             std::cout<<"Incorrect datas. Repeat please"<<"\n";
-        }else continue
+            breake;
     }
     
+    if(phoneBook.find(name)!=phoneBook.end()){
+            std::cout<<"There is the same Name\n";
+            break;
+    }else{
+        phoneBook.insert(std::make_pair(name, phone))
+        break;
+    }
     
     }
-	void call(std::map<std::string, std::string> phoneBook){
-	std:: string name;
 	
-	}
+	void call(std::string> phoneBook){
+	
+    if(phoneBook.find(name)!=phoneBook.end()){
+        std::<<"Calling number: "<<phoneBook[name]<<std::endl;
+	}else{
+        std::cout<<"Contact wasn't found"<<std::endl;
+    }
+}
 
-	void sms(std::map<std::string, std::string> phoneBook){
+	void sms(std::string name, std::string message){
+        
+        if(phoneBook.find(name)!=phoneBook.end()){
+        std::<<"Sending message "<<message<<" "<<phoneBook[name]<<std::endl;
+	}else{
+        std::cout<<"Contact wasn't found"<<std::endl;
+    }
+        
+        
+        
 	
 	}
 };
